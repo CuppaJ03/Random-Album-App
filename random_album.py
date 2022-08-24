dca2b459826ce31c204c8a7fa30af36adfed7456
CuppaJ03 <75274424+CuppaJ03@users.noreply.github.com>
2022-08-24 15:54:23 -0400

    First Edition
    Basic command line selection with 15 albums

---
+++ random_album.py
@@ -0,0 +1,12 @@
+## RANDOM ALBUM SELECTOR ##
+
+import random
+
+#picking out the albums into a list
+myAlbums = []
+f = open('album_list.txt','r')
+for line in f:
+    myAlbums.append(line.strip())
+    
+    
+print('\n', random.choice(myAlbums), '\n')
